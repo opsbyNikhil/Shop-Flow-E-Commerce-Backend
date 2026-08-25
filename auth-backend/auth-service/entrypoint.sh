81a36aeb3bd70e0daf422c2a64396ada2a4efdeb
opsbyNikhil <purposejob97@gmail.com>
2026-08-25 13:48:27 +0530

    entrypoint

--- auth-backend/auth-service/entrypoint.sh
+++ auth-backend/auth-service/entrypoint.sh
@@ -4,8 +4,28 @@ set -e
 echo "Running databsae migrations....."
 python manage.py migrate --noinput
 
-echo "Running database migrations....."
-python manage.py migrate --noinput
+echo "Creating superuser if it does not exist....."
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "shopflow-auth"
+email = ""
+password = "shopflow_auth@12345"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists.")
+EOF
+
 
 echo "Running static files....."
 python manage.py collectstatic --noinput
--- profile-service/entrypoint.sh
+++ profile-service/entrypoint.sh
@@ -4,11 +4,31 @@ set -e
 echo "Running databsae migrations....."
 python manage.py migrate --noinput
 
-echo "Running database migrations....."
-python manage.py migrate --noinput
+echo "Creating superuser if it does not exist....."
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = "shopflow-profile"
+email = ""
+password = "shopflow_profiles@12345"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+    print("Superuser created successfully.")
+else:
+    print("Superuser already exists.")
+EOF
+
 
 echo "Running static files....."
 python manage.py collectstatic --noinput
 
 echo "Running Django Server....."
-exec python manage.py runserver 0.0.0.0:8002
+exec python manage.py runserver 0.0.0.0:8005

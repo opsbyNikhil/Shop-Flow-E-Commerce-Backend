32dedc64eb3f939c1dfb84d5bfd4774f0f678893
opsbyNikhil <purposejob97@gmail.com>
2026-08-31 13:27:43 +0530

    .\settings.py

--- cart-backend/cart-service/cart_service/settings.py
+++ cart-backend/cart-service/cart_service/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["13.215.86.33","127.0.0.1", "localhost"]
-
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 # Application definition
 
@@ -145,12 +147,12 @@ MAILERS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://18.142.2.14:5173",
-    "http://18.142.2.14:5174",
-    "http://18.142.2.14:5175",
-    "http://18.142.2.14:5176",
-    "http://18.142.2.14:5177",
-    "http://18.142.2.14:5178",
+    "http://3.0.235.84:5173",
+    "http://3.0.235.84:5174",
+    "http://3.0.235.84:5175",
+    "http://3.0.235.84:5176",
+    "http://3.0.235.84:5177",
+    "http://3.0.235.84:5178",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
